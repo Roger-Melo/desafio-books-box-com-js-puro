daf473fc31c50ddfba1d370b5d1028b8d42bd4d8
Roger Melo <rogerw592@gmail.com>
2021-05-14 19:17:40 -0300

    Add challenge description

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+  - O objeto abaixo representa uma caixa de livros com espaço para 5 livros e 
+    que, no momento em que foi declarado, possui nenhum livro dentro;
+  - Crie um método que irá adicionar livros na caixa;
+  
+  Este método deve:
+    - Receber por parâmetro o número de livros que serão colocados na caixa. 
+      Esse número não precisa encher a caixa de uma só vez, os livros podem 
+      ser acrescentados aos poucos;
+    - Retornar a frase: "Já há 'X' livros na caixa";
+    - Se a caixa já estiver cheia, com todos os espaços já preenchidos, o 
+      método deve retornar a frase: "A caixa já está cheia";
+    - Se ainda houverem espaços na caixa mas a quantidade de livros passada por
+      parâmetro for ultrapassar o limite de espaços da caixa, mostre quantos 
+      espaços ainda podem ser ocupados, com a frase: "Só cabem mais 
+      QUANTIDADE_DE_LIVROS_QUE_CABEM livros";
+    - Se couber somente mais um livro, mostre a palavra "livro" (no singular) 
+      na frase acima.
+*/
+
+let booksBox = {
+  spaces: 5,
+  booksIn: 0
+}
